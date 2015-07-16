26d14bcd5097dbf5e2e8e5033c518c11cfd5e22d
Mike Fikes <mike@fikesfarm.com>
2015-07-16 06:46:24 -0400

    Initial Xcode project

---
+++ planck/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  planck
+//
+//  Created by Mike Fikes on 7/16/15.
+//  Copyright (c) 2015 FikesFarm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
