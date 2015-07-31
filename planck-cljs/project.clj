6c927e78f39726a8cf29e2795d831443c038bdd9
Mike Fikes <mike@fikesfarm.com>
2015-07-31 13:21:47 -0400

    Latest deps

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,5 +1,5 @@
 (defproject planck "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.10"]
+                 [org.clojure/clojurescript "1.7.28"]
                  [org.omcljs/ambly "0.6.0"]]
   :clean-targets ["out" "target"])
