bd71690eb423eee71f8247d6b025dee3a9b2bffa
Mike Fikes <mike@fikesfarm.com>
2015-07-31 21:01:40 -0400

    WIP on build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set up Xcode project (currently requires CocoaPods)
+pod install
+
+# Build ClojureScript (currently requires lein)
+cd planck-cljs
+script/build
+script/bundle
+cd ..
+
+# Xcode
+xcodebuild -workspace planck.xcworkspace -scheme planck
