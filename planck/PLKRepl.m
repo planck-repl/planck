44eccfe84b93f80bf4fe0db4fc7497cc7fa07ee2
Mike Fikes <mike@fikesfarm.com>
2015-08-03 23:17:23 -0400

    Clean up executive and REPL code
    Former-commit-id: f87ed4200fbee4c834f4550674bb7ec2f3335196

--- planck/PLKRepl.m
+++ planck/PLKRepl.m
@@ -6,6 +6,7 @@
 static PLKClojureScriptEngine* s_clojureScriptEngine = nil;
 
 void completion(const char *buf, linenoiseCompletions *lc) {
+    
     NSArray* completions = [s_clojureScriptEngine getCompletionsForBuffer:[NSString stringWithCString:buf
                                                                                              encoding:NSUTF8StringEncoding]];
     for (NSString* completion in completions) {
@@ -18,13 +19,16 @@ void completion(const char *buf, linenoiseCompletions *lc) {
 -(NSString*)formPrompt:(NSString*)currentNs isSecondary:(BOOL)secondary
 {
     if (secondary) {
-        return [[@"" stringByPaddingToLength:currentNs.length-2 withString:@" " startingAtIndex:0] stringByAppendingString:@"#_=> "];
+        return [[@"" stringByPaddingToLength:currentNs.length-2
+                                  withString:@" "
+                             startingAtIndex:0]
+                stringByAppendingString:@"#_=> "];
     } else {
         return [NSString stringWithFormat:@"%@=> ", currentNs];
     }
 }
 
--(NSString *) getInput
+-(NSString *)getInput
 {
     NSFileHandle *input = [NSFileHandle fileHandleWithStandardInput];
     NSData *inputData = [input availableData];
@@ -36,78 +40,89 @@ void completion(const char *buf, linenoiseCompletions *lc) {
     }
 }
 
+-(void)displayPrompt:(NSString*)prompt
+{
+    printf("%s", [prompt cStringUsingEncoding:NSUTF8StringEncoding]);
+    fflush(stdout);
+}
+
 -(void)runPlainTerminal:(BOOL)plainTerminal usingClojureScriptEngine:(PLKClojureScriptEngine*)clojureScriptEngine
 {
-    if (plainTerminal) {
-        printf("cljs.user=> ");
-        fflush(stdout);
-    }
-    
+    NSString* currentNs = @"cljs.user";
+    NSString* currentPrompt = [self formPrompt:currentNs isSecondary:NO];
     NSString* historyFile = nil;
+    
     if (!plainTerminal) {
+    
         char* homedir = getenv("HOME");
         if (homedir) {
             historyFile = [NSString stringWithFormat:@"%@/.planck_history", [NSString stringWithCString:homedir encoding:NSUTF8StringEncoding]];
             linenoiseHistoryLoad([historyFile cStringUsingEncoding:NSUTF8StringEncoding]);
         }
-    }
-    
-    // Set up linenoise prompt
-    NSString* currentNs = @"cljs.user";
-    NSString* currentPrompt = @"";
-    if (!plainTerminal) {
-        currentPrompt = [self formPrompt:currentNs isSecondary:NO];
-    }
-    
-    NSString* input = nil;
-    char *line = NULL;
-    
-    if (!plainTerminal) {
+        
         linenoiseSetMultiLine(1);
+        
         s_clojureScriptEngine = clojureScriptEngine;
         linenoiseSetCompletionCallback(completion);
     }
     
-    while(plainTerminal || (line = linenoise([currentPrompt cStringUsingEncoding:NSUTF8StringEncoding])) != NULL) {
+    BOOL clojureScriptEnginePrintsNils = NO;
+    
+    NSString* input = nil;
+    char *line = NULL;
+    while(plainTerminal ||
+          (line = linenoise([currentPrompt cStringUsingEncoding:NSUTF8StringEncoding])) != NULL) {
+
+        // Get the current line of input
         
         NSString* inputLine;
-        
-        if (!plainTerminal) {
-            inputLine = [NSString stringWithCString:line encoding:NSUTF8StringEncoding];
-            free(line);
-        } else {
+
+        if (plainTerminal) {
+            [self displayPrompt:currentPrompt];
             inputLine = [self getInput];
-            // Check if ^D has been pressed and if so, exit
-            if (inputLine == nil) {
+            if (inputLine == nil) { // ^D has been pressed
                 printf("\n");
                 break;
             }
+        } else {
+            inputLine = [NSString stringWithCString:line encoding:NSUTF8StringEncoding];
+            free(line);
         }
                 
-        // TODO arrange to have this occur only once
-        [clojureScriptEngine setAllowPrintNils];
+        if (!clojureScriptEnginePrintsNils) {
+            [clojureScriptEngine setAllowPrintNils];
+            clojureScriptEnginePrintsNils = YES;
+        }
+        
+        // Accumulate input lines
         
         if (input == nil) {
             input = inputLine;
-            if (!plainTerminal) {
-                currentPrompt = [self formPrompt:currentNs isSecondary:YES];
-            }
         } else {
             input = [NSString stringWithFormat:@"%@\n%@", input, inputLine];
         }
         
+        // Check for explicit exit
+        
         if ([input isEqualToString:@":cljs/quit"]) {
             break;
-        } else {
-            if (!plainTerminal) {
-                linenoiseHistoryAdd([inputLine cStringUsingEncoding:NSUTF8StringEncoding]);
-                if (historyFile) {
-                    linenoiseHistorySave([historyFile cStringUsingEncoding:NSUTF8StringEncoding]);
-                }
+        }
+        
+        // Add input line to history
+        
+        if (!plainTerminal) {
+            linenoiseHistoryAdd([inputLine cStringUsingEncoding:NSUTF8StringEncoding]);
+            if (historyFile) {
+                linenoiseHistorySave([historyFile cStringUsingEncoding:NSUTF8StringEncoding]);
             }
         }
         
+        // Check if we now have a readable form
+        // and if so, evaluate it.
+        
         if ([clojureScriptEngine isReadable:input]) {
+            
+            // Guard against empty string being read
             
             NSCharacterSet *charSet = [NSCharacterSet whitespaceCharacterSet];
             NSString *trimmedString = [input stringByTrimmingCharactersInSet:charSet];
@@ -119,18 +134,22 @@ void completion(const char *buf, linenoiseCompletions *lc) {
                 }
             }
             
+            // Now that we've evaluated the input, reset for next round
+            
             input = nil;
             
-            currentNs = [clojureScriptEngine getCurrentNs];
-            if (!plainTerminal) {
-                currentPrompt = [self formPrompt:currentNs isSecondary:NO];
-            } else {
-                printf("%s", [currentNs cStringUsingEncoding:NSUTF8StringEncoding]);
-                printf("=> ");
-                fflush(stdout);
-            }
+            // Fetch the current namespace and use it to set the prompt
             
+            currentNs = [clojureScriptEngine getCurrentNs];
+            currentPrompt = [self formPrompt:currentNs isSecondary:NO];
+            
+        } else {
+            
+            // Prepare for reading non-1st line of input with secondary prompt
+            
+            currentPrompt = [self formPrompt:currentNs isSecondary:YES];
         }
+        
     }
     
 }
