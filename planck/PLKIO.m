a698e5de2b9f4cd93e2e141b99645006b9c6621d
Benedikt Terhechte <terhechte@gmail.com>
2015-08-12 15:26:16 +0200

    Added file-seq obj-c implementation
    Former-commit-id: 734767f0c988c9fdcd206a90cfc7e222bafc878a

--- planck/PLKIO.m
+++ planck/PLKIO.m
@@ -34,3 +34,46 @@
 }
 @end
 
+NSArray *cljs_file_seq(PLKFile *input) {
+    // Create a local file manager instance
+    NSFileManager *localFileManager=[[NSFileManager alloc] init];
+    
+    BOOL isDirectory = NO;
+    BOOL exists = [localFileManager fileExistsAtPath:input.path isDirectory:&isDirectory];
+    
+    
+    if (!isDirectory || !exists) {
+        NSLog(@"Error: file isn't a directory or doesn't exist");
+        return @[];
+    }
+    
+    // Request the two properties the method uses, name and isDirectory
+    // Ignore hidden files
+    NSURL *inputURL = [NSURL fileURLWithPath:input.path];
+    NSDirectoryEnumerator *dirEnumerator = [localFileManager enumeratorAtURL:inputURL
+                                                  includingPropertiesForKeys:[NSArray arrayWithObjects:NSURLNameKey,
+                                                                              NSURLIsDirectoryKey,nil]
+                                                                     options:NSDirectoryEnumerationSkipsHiddenFiles
+                                                                errorHandler:nil];
+    
+    // An array to store the all the enumerated file names in
+    NSMutableArray *theArray=[NSMutableArray array];
+    
+    // Enumerate the dirEnumerator results, each value is stored in allURLs
+    for (NSURL *theURL in dirEnumerator) {
+        
+        // Retrieve the file name. From NSURLNameKey, cached during the enumeration.
+        NSString *fileName;
+        [theURL getResourceValue:&fileName forKey:NSURLNameKey error:NULL];
+        
+        // Retrieve whether a directory. From NSURLIsDirectoryKey, also
+        // cached during the enumeration.
+        NSNumber *isDirectory;
+        [theURL getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:NULL];
+        
+        [theArray addObject: [PLKFile file:theURL.path]];
+    }
+    
+    return theArray.copy;
+}
+
