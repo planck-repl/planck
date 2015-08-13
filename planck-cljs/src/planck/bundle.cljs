f3c3671f0027b7a1a856703362a5cfe80a2f16a1
Mike Fikes <mike@fikesfarm.com>
2015-08-13 11:55:05 -0400

    Add a mechanism to bundle unused things
    WIP on #56

---
+++ planck-cljs/src/planck/bundle.cljs
@@ -0,0 +1,9 @@
+;; A namespace used to cause additional libs to be included in out for bundling.
+(ns planck.bundle
+  (:require
+    cljs.pprint
+    cljs.test
+    goog.date.Date
+    goog.date.DateTime
+    goog.date.UtcDateTime
+    goog.i18n.TimeZone))
