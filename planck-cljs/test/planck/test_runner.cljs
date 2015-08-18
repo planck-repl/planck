92545bce503157a6d89b92b8ab1838f3155fed2a
Mike Fikes <mike@fikesfarm.com>
2015-08-18 15:46:25 -0400

    Hook in cljs.test capability

---
+++ planck-cljs/test/planck/test_runner.cljs
@@ -0,0 +1,13 @@
+(ns planck.test-runner
+  (:require [cljs.test :refer-macros [run-tests]]
+            [planck.core-test]
+            [planck.io-test]
+            [planck.shell-test]
+            [planck.repl-test]))
+
+(defn run-all-tests []
+  (run-tests
+    'planck.core-test
+    'planck.io-test
+    'planck.shell-test
+    'planck.repl-test))
