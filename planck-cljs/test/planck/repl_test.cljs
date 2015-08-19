0d636cd781a2a90152295b3dd4a05e8c21bdb122
Mike Fikes <mike@fikesfarm.com>
2015-08-18 20:22:44 -0400

    Unit tests for get-highlight-coords

--- planck-cljs/test/planck/repl_test.cljs
+++ planck-cljs/test/planck/repl_test.cljs
@@ -2,6 +2,15 @@
   (:require [cljs.test :refer-macros [deftest testing is]]
             [planck.repl]))
 
-(deftest a-test
-  (testing "Stub passing test"
-    (is (= 0 0))))
+(deftest get-highlight-coords
+  (testing "Highlight coordinates"
+    (is (= [0 0] (js->clj (planck.repl/get-highlight-coords 1 "[]" []))))
+    (is (= [0 1] (js->clj (planck.repl/get-highlight-coords 2 "[[]]" []))))
+    (is (= [0 0] (js->clj (planck.repl/get-highlight-coords 3 "[()]" []))))
+    (is (= [0 1] (js->clj (planck.repl/get-highlight-coords 2 " []" []))))
+    (is (= [1 0] (js->clj (planck.repl/get-highlight-coords 1 "]" ["["]))))
+    (is (= [2 0] (js->clj (planck.repl/get-highlight-coords 1 "]" ["[" ""]))))
+    (is (= [1 0] (js->clj (planck.repl/get-highlight-coords 1 "]" ["[" "["]))))
+    (is (= [2 0] (js->clj (planck.repl/get-highlight-coords 1 "]" ["[" "[]"]))))
+    (is (= [0 1] (js->clj (planck.repl/get-highlight-coords 2 "#{}" []))))
+    (is (= [0 0] (js->clj (planck.repl/get-highlight-coords 4 "[\"[\"]" []))))))
