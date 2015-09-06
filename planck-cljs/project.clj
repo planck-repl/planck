01cd301dbcbed793e0d0c05d64874a9221662e2e
Johann Bestowrous <johann.bestowrous@gmail.com>
2015-09-06 16:30:50 -0700

    fix cljs.tools.reader dependency issue

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
                  [tailrecursion/cljson "1.0.7"]
+                 [org.clojure/tools.reader "0.10.0-alpha3"]
                  [com.cognitect/transit-clj "0.8.275"]
                  [com.cognitect/transit-cljs "0.8.220"]]
   :clean-targets ["out" "target"])
