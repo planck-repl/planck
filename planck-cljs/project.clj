a06b414a9108f4a6a34edccb05599572cdadede2
Mike Fikes <mike@fikesfarm.com>
2015-11-06 09:06:02 -0500

    Update to ClojureScript 1.7.170

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,6 +1,6 @@
 (defproject planck "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.166"]
+                 [org.clojure/clojurescript "1.7.170"]
                  [tailrecursion/cljson "1.0.7"]
                  [com.cognitect/transit-clj "0.8.275"]
                  [com.cognitect/transit-cljs "0.8.220"]]
