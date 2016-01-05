704c512acb9504a7fc36730e78cb1e3b6c26bfc7
Mike Fikes <mike@fikesfarm.com>
2016-01-05 16:43:09 -0500

    Update to latest tools.reader for TRDR-33 (syntax-quote)

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,6 +1,7 @@
 (defproject planck "0.1.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.189"]
+                 [org.clojure/tools.reader "1.0.0-alpha3"]
                  [tailrecursion/cljson "1.0.7"]
                  [com.cognitect/transit-clj "0.8.275"]
                  [com.cognitect/transit-cljs "0.8.220"]
