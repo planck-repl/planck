5d53f056f48c17a5d3d9586bbf8b33e00bce5927
Mikkel Gravgaard <mgn@skm.dk>
2016-01-10 20:27:37 +0100

    mention namespace in help

--- planck/PLKCommandLine.m
+++ planck/PLKCommandLine.m
@@ -260,7 +260,7 @@
             printf("  operation:\n");
             printf("\n");
             printf("    - Enters the cljs.user namespace\n");
-            printf("    - Binds *command-line-args* to a seq of strings containing command line\n");
+            printf("    - Binds planck.core/*command-line-args* to a seq of strings containing command line\n");
             printf("      args that appear after any main option\n");
             printf("    - Runs all init options in order\n");
             printf("    - Calls a -main function or runs a repl or script if requested\n");
