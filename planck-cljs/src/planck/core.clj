bb59008bf06eb7d989e486cbc9ccc2572063df63
Erik Assum <erik@assum.net>
2016-01-24 22:00:59 +0100

    Add a with-open macro, fixes #57
    - implement with-open from clojure.core
    - add planck/core.clj to the build

---
+++ planck-cljs/src/planck/core.clj
@@ -0,0 +1,24 @@
+(ns planck.core)
+
+(defmacro with-open
+  "bindings => [name IClosable ...]
+
+  Evaluates body in a try expression with names bound to the values
+  of the inits, and a finally clause that calls (-close name) on each
+  name in reverse order."
+  [bindings & body]
+  ;; when http://dev.clojure.org/jira/browse/CLJS-1551 lands,
+  ;; replace with assert-args
+  (when-not (vector? bindings)
+    (throw (js/Error. "with-open requires a vector for its bindings")))
+  (when-not (even? (count bindings))
+    (throw (js/Error. "with-open requires an even number of forms in binding vector")))
+  (cond
+    (= (count bindings) 0) `(do ~@body)
+    (symbol? (bindings 0)) `(let ~(subvec bindings 0 2)
+                              (try
+                                (with-open ~(subvec bindings 2) ~@body)
+                                (finally
+                                  (-close ~(bindings 0)))))
+    :else (throw (js/Error.
+                  "with-open only allows symbols in bindings"))))
