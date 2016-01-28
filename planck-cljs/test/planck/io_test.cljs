ab7d12456e1c4727e9ecb26e01728eb16e27e028
Mike Fikes <mike@fikesfarm.com>
2016-01-28 17:31:08 -0500

    Unit tests for planck.io/fstat

--- planck-cljs/test/planck/io_test.cljs
+++ planck-cljs/test/planck/io_test.cljs
@@ -2,6 +2,25 @@
   (:require [cljs.test :refer-macros [deftest testing is]]
             [planck.io]))
 
-(deftest a-test
-  (testing "Stub passing test"
-    (is (= 0 0))))
+(deftest fstat-test
+  (testing "fstat"
+    (is (nil? (planck.io/fstat nil)))
+    (is (nil? (planck.io/fstat "bogus")))
+    (is (map? (planck.io/fstat "/tmp")))
+    (is (keyword-identical? :symboliclink (:type (planck.io/fstat "/tmp"))))
+    (is (keyword-identical? :symboliclink (:type (planck.io/fstat (planck.io/file "/tmp")))))
+    (is (number? (:referencecount (planck.io/fstat "/tmp"))))
+    (is (number? (:permissions (planck.io/fstat "/tmp"))))
+    (is (number? (:filenumber (planck.io/fstat "/tmp"))))
+    (is (number? (:uid (planck.io/fstat "/tmp"))))
+    (is (number? (:gid (planck.io/fstat "/tmp"))))
+    (is (string? (:uname (planck.io/fstat "/tmp"))))
+    (is (string? (:gname (planck.io/fstat "/tmp"))))
+    (is (= js/Date (type (:created (planck.io/fstat "/tmp")))))
+    (is (= js/Date (type (:modified (planck.io/fstat "/tmp")))))
+    (is (number? (:creatorcode (planck.io/fstat "/tmp"))))
+    (is (number? (:typecode (planck.io/fstat "/tmp"))))
+    (is (number? (:filesize (planck.io/fstat "/tmp"))))
+    (let [extension-hidden (:extensionhidden (planck.io/fstat "/tmp"))]
+      (is (or (true? extension-hidden)
+              (false? extension-hidden))))))
