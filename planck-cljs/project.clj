3d129f6417a46d001aa2609c0e2a3c070283e9cd
Mike Fikes <mike@fikesfarm.com>
2016-02-24 09:45:39 -0500

    Update to master of tools.reader
    Fixes #218

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,7 +1,7 @@
 (defproject planck "0.1.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  #_[org.clojure/clojurescript "1.7.228"]
-                 [org.clojure/tools.reader "1.0.0-alpha3"]
+                 [org.clojure/tools.reader "1.0.0-SNAPSHOT"]
                  [tailrecursion/cljson "1.0.7"]
                  [com.cognitect/transit-clj "0.8.275"]
                  [com.cognitect/transit-cljs "0.8.220"]
