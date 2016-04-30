113f23a0425d8352e110809428afa7c29ab03847
Mike Fikes <mike@fikesfarm.com>
2016-04-30 12:47:44 -0400

    Update deps and set up for dev/build

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,9 +1,12 @@
 (defproject planck "0.1.0"
+  :profiles {:dev
+             {:dependencies [[org.clojure/clojurescript "1.8.51"]
+                             [tubular "1.0.0"]]}
+             :build
+             {:dependencies [[com.cognitect/transit-clj "0.8.275"]]}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 #_[org.clojure/clojurescript "1.7.228"]
-                 [org.clojure/tools.reader "1.0.0-alpha4"]
+                 [org.clojure/tools.reader "1.0.0-beta1"]
                  [tailrecursion/cljson "1.0.7"]
-                 [com.cognitect/transit-clj "0.8.275"]
                  [com.cognitect/transit-cljs "0.8.220"]
                  [malabarba/lazy-map "1.1"]
                  [cljsjs/parinfer "1.5.1-0"]]
