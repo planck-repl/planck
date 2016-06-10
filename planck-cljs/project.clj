595e24898bdd8010163639a441899e5945cb6a55
Mike Fikes <mike@fikesfarm.com>
2016-06-10 12:31:57 -0400

    Set to build against ClojureScript master

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,11 +1,11 @@
 (defproject planck "0.1.0"
   :profiles {:dev
-             {:dependencies [#_[org.clojure/clojurescript "1.9.36"]
+             {:dependencies [[org.clojure/clojurescript "1.9.36"]
                              [tubular "1.0.0"]]
               :source-paths ["dev"]}
              :build {}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.36"]       ; comment if building own
+                 #_[org.clojure/clojurescript "1.9.36"]       ; comment if building own, and revise planck-cljs/script/build
                  [org.clojure/tools.reader "1.0.0-beta1"]
                  [tailrecursion/cljson "1.0.7"]
                  [com.cognitect/transit-clj "0.8.285"]
