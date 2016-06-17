580e6b212b33e228df3287173bcce151c8aa7fef
Lucas Stadler <lu@papill0n.org>
2016-06-17 23:14:36 +0200

    Use nanosleep on all systems (#319)

--- planck-c/repl.c
+++ planck-c/repl.c
@@ -264,11 +264,7 @@ void *do_highlight_restore(void *data) {
 		struct timespec t;
 		t.tv_sec = 0;
 		t.tv_nsec = *timeout;
-#ifdef CLOCK_REALTIME
-		clock_nanosleep(CLOCK_REALTIME, 0, &t, NULL);
-#else
 		nanosleep(&t, NULL);
-#endif
 	}
 
 	if (hl_restore.num_lines_up != 0) {
