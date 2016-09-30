f15ea541a5f99cf7ff69747e028b3d0798753d9e
Mike Fikes <mike@fikesfarm.com>
2016-09-30 15:52:08 -0400

    Update to use transit-clj 0.8.290 during build

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -8,7 +8,7 @@
                  [org.clojure/clojurescript "1.9.229"]       ; comment if building own, and revise planck-cljs/script/build
                  [org.clojure/tools.reader "1.0.0-beta3"]
                  [tailrecursion/cljson "1.0.7"]
-                 [com.cognitect/transit-clj "0.8.285"]
+                 [com.cognitect/transit-clj "0.8.290"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [fipp "0.6.6"]
                  [malabarba/lazy-map "1.1"]
