85a2de1de74e5f2069b5381a20bd872d86526ef7
Mike Fikes <mike@fikesfarm.com>
2016-12-11 22:28:41 -0500

    Minor cleanup

--- planck-cljs/test/planck/setup_eval.cljs
+++ planck-cljs/test/planck/setup_eval.cljs
@@ -1,7 +1,7 @@
 (ns planck.setup-eval
   (:require-macros [cljs.spec.test])
-  (:require [planck.core :refer [eval]]))
+  (:require [planck.core :refer [eval intern]]))
 
 ;; The cljs.spec.test macros namespace makes use of eval.
 ;; Intern Planck's implementation of eval for use there.
-(planck.core/intern 'cljs.spec.test$macros 'eval eval)
+(intern 'cljs.spec.test$macros 'eval eval)
