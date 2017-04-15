41d08645a4881ec42a016b19e0c5c61171170d91
Mike Fikes <mike@fikesfarm.com>
2017-04-14 21:47:56 -0400

    Update to ClojureScript 1.9.521

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,6 +1,6 @@
 (defproject planck "0.1.0"
   :profiles {:dev
-             {:dependencies [[org.clojure/clojurescript "1.9.518"]
+             {:dependencies [[org.clojure/clojurescript "1.9.521"]
                              [org.clojure/test.check "0.9.0"]
                              [tubular "1.0.0"]]
               :source-paths ["dev"]}
@@ -9,7 +9,7 @@
              :build-commit
              {}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.518"]
+                 [org.clojure/clojurescript "1.9.521"]
                  [org.clojure/test.check "0.9.0"]
                  [org.clojure/tools.reader "1.0.0-beta4"]
                  [tailrecursion/cljson "1.0.7"]
