91f10c31ec3f803b32ebe831b3107d7e8bb49801
Mike Fikes <mike@fikesfarm.com>
2017-05-27 23:27:34 -0400

    Update tools.reader to 1.0.0-RC1

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -11,7 +11,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.562"]
                  [org.clojure/test.check "0.9.0"]
-                 [org.clojure/tools.reader "1.0.0-beta4"]
+                 [org.clojure/tools.reader "1.0.0-RC1"]
                  [com.cognitect/transit-clj "0.8.297"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [fipp "0.6.7"]
