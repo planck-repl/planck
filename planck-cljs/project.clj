09ffc6abea71fcb1a1a4dd571fca3d56a7f4d365
Mike Fikes <mike@fikesfarm.com>
2017-07-11 17:27:44 -0400

    Update to test.check-0.10.0-alpha2

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,7 +1,7 @@
 (defproject planck "0.1.0"
   :profiles {:dev
              {:dependencies [[org.clojure/clojurescript ~(or (System/getenv "CLOJURESCRIPT_VERSION") "1.9.660")]
-                             [org.clojure/test.check "0.10.0-alpha1"]
+                             [org.clojure/test.check "0.10.0-alpha2"]
                              [tubular "1.0.0"]]
               :source-paths ["dev"]}
              :build-release 
@@ -10,7 +10,7 @@
              {}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript ~(or (System/getenv "CLOJURESCRIPT_VERSION") "1.9.660")]
-                 [org.clojure/test.check "0.10.0-alpha1"]
+                 [org.clojure/test.check "0.10.0-alpha2"]
                  [org.clojure/tools.reader "1.0.2"]
                  [com.cognitect/transit-clj "0.8.300"]
                  [com.cognitect/transit-cljs "0.8.239"]
