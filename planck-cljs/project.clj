9892b3a01f4e4f9490830c65bc6655c1a1c4edca
Mike Fikes <mike@fikesfarm.com>
2017-07-15 15:47:09 -0400

    Look for Canary ClojureScript version

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -1,6 +1,7 @@
+(def clojurescript-version (or (System/getenv "CANARY_CLOJURESCRIPT_VERSION") "1.9.660"))
 (defproject planck "0.1.0"
   :profiles {:dev
-             {:dependencies [[org.clojure/clojurescript ~(or (System/getenv "CLOJURESCRIPT_VERSION") "1.9.660")]
+             {:dependencies [[org.clojure/clojurescript ~clojurescript-version]
                              [org.clojure/test.check "0.10.0-alpha2"]
                              [tubular "1.0.0"]]
               :source-paths ["dev"]}
@@ -9,7 +10,7 @@
              :build-commit
              {}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript ~(or (System/getenv "CLOJURESCRIPT_VERSION") "1.9.660")]
+                 [org.clojure/clojurescript ~clojurescript-version]
                  [org.clojure/test.check "0.10.0-alpha2"]
                  [org.clojure/tools.reader "1.0.0"]
                  [com.cognitect/transit-clj "0.8.300"]
