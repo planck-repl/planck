ccf554cdc7f33004205e7162f46a714ba8aab358
Mike Fikes <mike@fikesfarm.com>
2017-07-19 10:30:52 -0400

    Test new *command-line-args* var

--- int-test/src/test_args/args_in_file.cljs
+++ int-test/src/test_args/args_in_file.cljs
@@ -2,3 +2,6 @@
   (:require planck.core))
 
 (println planck.core/*command-line-args*)
+(if (exists? *command-line-args*)
+  (println ^:cljs.analyzer/no-resolve *command-line-args*)
+  (println planck.core/*command-line-args*))
