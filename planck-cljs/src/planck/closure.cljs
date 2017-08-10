448ffabda9527ccbbb7b845999c6afe103453ac4
Mike Fikes <mike@fikesfarm.com>
2017-08-09 23:25:17 -0400

    Throw on Closure errors, refactor

--- planck-cljs/src/planck/closure.cljs
+++ planck-cljs/src/planck/closure.cljs
@@ -3,37 +3,56 @@
   (:require
    [cljs.source-map :as sm]))
 
-(defn compile
-  "Uses Closure to compile JavaScript source. If :sm-data is supplied, a
-  composed :source-map will calculated and be returned in the result."
-  [{:keys [name source sm-data optimizations verbose]
-    :or {optimizations :simple}}]
+(defn- call-compiler
+  [name source sm-data optimizations verbose]
   (when-not (exists? js/compile)
     (js/AMBLY_IMPORT_SCRIPT "jscomp.js"))
   (when verbose
     (println "Applying" optimizations "Closure optimizations to" name))
-  (let [result (js/compile #js {:jsCode                   #js [#js {:src source}]
-                                :compilationLevel         (case optimizations
-                                                            :simple "SIMPLE"
-                                                            :whitespace "WHITESPACE_ONLY")
-                                :languageIn               "ECMASCRIPT3"
-                                :languageOut              "ECMASCRIPT3"
-                                :processClosurePrimitives false
-                                :createSourceMap          (some? sm-data)
-                                :applyInputSourceMaps     false})]
-    (when-not (empty? (.-errors result))
-      (prn (.-errors result)))
-    (when-not (empty? (.-warnings result))
-      (prn (.-warnings result)))
-    (if (.-compiledCode result)
-      (merge {:source (.-compiledCode result)}
-        (when (some? sm-data)
-          {:source-map (->> result
-                         .-sourceMap
-                         (.parse js/JSON)
-                         sm/decode-reverse
-                         vals
-                         first
-                         (sm/merge-source-maps (:source-map sm-data))
-                         sm/invert-reverse-map)}))
-      {:source source})))
+  (try
+    (js/compile #js {:jsCode                   #js [#js {:src source}]
+                     :compilationLevel         (case optimizations
+                                                 :simple "SIMPLE"
+                                                 :whitespace "WHITESPACE_ONLY")
+                     :languageIn               "ECMASCRIPT3"
+                     :languageOut              "ECMASCRIPT3"
+                     :processClosurePrimitives false
+                     :createSourceMap          (some? sm-data)
+                     :applyInputSourceMaps     false})
+    (catch :default e
+      (throw (ex-info "Internal error running Closure compiler"
+               {:name name, :optimizations optimizations} e)))))
+
+(defn- check-compilation-results
+  [name optimizations results]
+  (when-not (empty? (.-warnings results))
+    (println "Closure compilation warnings" (.-warnings results)))
+  (when-not (empty? (.-errors results))
+    (println "Closure compilation errors" (.-errors results))
+    (throw (ex-info "Closure compilation errors" {:name          name
+                                                  :optimizations optimizations
+                                                  :errors        (.-errors results)}))))
+
+(defn- extract-results
+  [source sm-data results]
+  (if (.-compiledCode results)
+    (merge {:source (.-compiledCode results)}
+      (when (some? sm-data)
+        {:source-map (->> results
+                       .-sourceMap
+                       (.parse js/JSON)
+                       sm/decode-reverse
+                       vals
+                       first
+                       (sm/merge-source-maps (:source-map sm-data))
+                       sm/invert-reverse-map)}))
+    {:source source}))
+
+(defn compile
+  "Uses Closure to compile JavaScript source. If :sm-data is supplied, a
+  composed :source-map will calculated and be returned in the result."
+  [{:keys [name source sm-data optimizations verbose]
+    :or   {optimizations :simple}}]
+  (->> (call-compiler name source sm-data optimizations verbose)
+    (check-compilation-results name optimizations)
+    (extract-results source sm-data)))
