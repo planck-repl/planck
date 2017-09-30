77bc955228991ff22f91ed06576cd8b81db9370d
Mike Fikes <mike@fikesfarm.com>
2017-09-30 11:39:57 -0400

    Use tools.reader version specified by ClojureScript

--- planck-cljs/project.clj
+++ planck-cljs/project.clj
@@ -8,13 +8,10 @@
                              [tubular "1.0.0"]]
               :source-paths ["dev"]}
              :build-release 
-             {}
-             :build-commit
              {}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript ~clojurescript-version]
                  [org.clojure/test.check "0.10.0-alpha2"]
-                 [org.clojure/tools.reader "1.0.5"]
                  [com.cognitect/transit-clj "0.8.300"]
                  [com.cognitect/transit-cljs "0.8.239"]
                  [fipp "0.6.8"]
