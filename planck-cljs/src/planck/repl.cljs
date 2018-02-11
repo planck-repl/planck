planck-repl/planck

commit 97f1a0f47eb3b2f17a5c62ad9e74396566def0e8

Lazily load source maps at a per-ns granularity

planck-cljs/src/planck/repl.cljs
@@ -25,7 +25,7 @@
    [planck.pprint.code]
    [planck.pprint.data]
    [planck.pprint.width-adjust]
-   [planck.repl-resources :refer [repl-special-doc-map special-doc-map bundled-aot-namespaces]]
+   [planck.repl-resources :refer [repl-special-doc-map special-doc-map]]
    [planck.themes :refer [get-theme]]))
 
 (defn- distinct-by
@@ -1075,25 +1075,26 @@
     (run-main-impl *main-cli-fn* *command-line-args*)))
 
 (defn- load-bundled-source-maps!
-  []
+  [ns-syms]
   (when-not (get (:source-maps @planck.repl/st) 'cljs.core)
     (let [source-map-path  (fn [ns-sym]
                              (str (cljs.js/ns->relpath ns-sym) ".js.map"))
           load-source-maps (fn [ns-sym]
-                             (when-let [sm-text (->> ns-sym
-                                                  source-map-path
-                                                  js/PLANCK_LOAD
-                                                  first)]
-                               ;; Detect if we have source maps in need of decoding
-                               ;; or if they are AOT decoded.
-                               (if (or (string/starts-with? sm-text "{\"version\"")
-                                       (string/starts-with? sm-text "{\n\"version\""))
-                                 (cljs/load-source-map! st ns-sym sm-text)
-                                 (swap! st assoc-in [:source-maps ns-sym] (transit-json->cljs sm-text)))))]
-      ;; Source maps for bundled macros namespaces other than cljs.core are loaded
-      ;; via their cached ".js.map.json" file.
-      (load-source-maps 'cljs.core$macros)
-      (run! load-source-maps bundled-aot-namespaces))))
+                             (when-not (get-in [:source-maps ns-sym] @st)
+                               (when-let [sm-text (->> ns-sym
+                                                    source-map-path
+                                                    js/PLANCK_LOAD
+                                                    first)]
+                                 ;; Detect if we have source maps in need of decoding
+                                 ;; or if they are AOT decoded.
+                                 (if (or (string/starts-with? sm-text "{\"version\"")
+                                         (string/starts-with? sm-text "{\n\"version\""))
+                                   (cljs/load-source-map! st ns-sym sm-text)
+                                   (swap! st assoc-in [:source-maps ns-sym] (transit-json->cljs sm-text))))))]
+      (run! load-source-maps ns-syms))))
+
+(defn- load-core-macros-source-maps! []
+  (load-bundled-source-maps! '[cljs.core$macros]))
 
 (defonce ^:dynamic ^:private *planck-integration-tests* false)
 
@@ -1307,12 +1308,14 @@
      (when-let [data (and print-ex-data? (ex-data error))]
        (print-value data {::as-code? false}))
      (when include-stacktrace?
-       (load-bundled-source-maps!)
+       (load-core-macros-source-maps!)
        (let [canonical-stacktrace (st/parse-stacktrace
                                     {}
                                     (.-stack error)
                                     {:ua-product :safari}
-                                    {:output-dir "file://(/goog/..)?"})]
+                                    {:output-dir "file://(/goog/..)?"})
+             file->ns-sym         (fn [file] (symbol (string/replace (st/remove-ext file) "/" ".")))]
+         (load-bundled-source-maps! (distinct (map (comp file->ns-sym :file) canonical-stacktrace)))
          (println
            ((:ex-stack-fn theme)
             (mapped-stacktrace-str

planck-cljs/src/planck/repl_resources.cljs
@@ -118,270 +118,3 @@ itself (not its value) is returned. The reader macro #'x expands to (var x)."}})
                :doc      "Loads Clojure code from resources in classpath. A path is interpreted as
   classpath-relative if it begins with a slash or relative to the root
   directory for the current namespace otherwise."}})
-
-(def bundled-aot-namespaces
-  '[cljs.core
-    cljs.analyzer.api
-    cljs.compiler
-    cljs.env
-    cljs.js
-    cljs.pprint
-    cljs.reader
-    cljs.source-map
-    cljs.source-map.base64
-    cljs.source-map.base64-vlq
-    cljs.spec.alpha
-    cljs.spec.gen.alpha
-    cljs.spec.test.alpha
-    cljs.stacktrace
-    cljs.tagged-literals
-    cljs.test
-    cljs.tools.reader
-    cljs.tools.reader.impl.commons
-    cljs.tools.reader.impl.utils
-    cljs.tools.reader.reader-types
-    clojure.core.reducers
-    clojure.core.rrb-vector
-    clojure.core.rrb-vector.interop
-    clojure.core.rrb-vector.nodes
-    clojure.core.rrb-vector.protocols
-    clojure.core.rbb-vector.rbbt
-    clojure.core.rbb-vector.transients
-    clojure.core.rrb-vector.trees
-    clojure.data
-    clojure.set
-    clojure.string
-    clojure.walk
-    clojure.zip
-    cognitect.transit
-    fipp.clojure
-    fipp.deque
-    fipp.edn
-    fipp.ednize
-    fipp.visit
-    lazy-map.core
-    planck.core
-    planck.from.io.aviso.ansi
-    planck.http
-    planck.io
-    planck.js-deps
-    planck.pprint.code
-    planck.pprint.data
-    planck.pprint.width-adjust
-    planck.repl
-    planck.repl-resources
-    planck.shell
-    planck.themes
-    goog.array.array
-    goog.asserts.asserts
-    goog.async.delay
-    goog.async.freelist
-    goog.async.nexttick
-    goog.async.run
-    goog.async.throttle
-    goog.async.workqueue
-    goog.base
-    goog.color.alpha
-    goog.color.color
-    goog.color.names
-    goog.crypt.aes
-    goog.crypt.arc4
-    goog.crypt.base64
-    goog.crypt.basen
-    goog.crypt.blobhasher
-    goog.crypt.blockcipher
-    goog.crypt.cbc
-    goog.crypt.crypt
-    goog.crypt.hash
-    goog.crypt.hash32
-    goog.crypt.hashtester
-    goog.crypt.hmac
-    goog.crypt.md5
-    goog.crypt.pbkdf2
-    goog.crypt.sha1
-    goog.crypt.sha2
-    goog.crypt.sha224
-    goog.crypt.sha256
-    goog.crypt.sha2_64bit
-    goog.crypt.sha512
-    goog.crypt.sha512_256
-    goog.date.date
-    goog.date.datelike
-    goog.date.daterange
-    goog.date.duration
-    goog.date.relative
-    goog.date.relativewithplurals
-    goog.date.utcdatetime
-    goog.debug.console
-    goog.debug.debug
-    goog.debug.entrypointregistry
-    goog.debug.error
-    goog.debug.errorcontext
-    goog.debug.formatter
-    goog.debug.logbuffer
-    goog.debug.logger
-    goog.debug.logrecord
-    goog.debug.relativetimeprovider
-    goog.disposable.disposable
-    goog.disposable.idisposable
-    goog.dom.asserts
-    goog.dom.browserfeature
-    goog.dom.dom
-    goog.dom.htmlelement
-    goog.dom.nodetype
-    goog.dom.safe
-    goog.dom.tagname
-    goog.dom.tags
-    goog.events.browserevent
-    goog.events.browserfeature
-    goog.events.event
-    goog.events.eventid
-    goog.events.events
-    goog.events.eventtarget
-    goog.events.eventtype
-    goog.events.listenable
-    goog.events.listener
-    goog.events.listenermap
-    goog.format.emailaddress
-    goog.format.format
-    goog.format.htmlprettyprinter
-    goog.format.internationalizedemailaddress
-    goog.format.jsonprettyprinter
-    goog.fs.entry
-    goog.fs.entryimpl
-    goog.fs.error
-    goog.fs.filereader
-    goog.fs.filesaver
-    goog.fs.filesystem
-    goog.fs.filesystemimpl
-    goog.fs.filewriter
-    goog.fs.fs
-    goog.fs.progressevent
-    goog.fs.url
-    goog.functions.functions
-    goog.html.safehtml
-    goog.html.safescript
-    goog.html.safestyle
-    goog.html.safestylesheet
-    goog.html.safeurl
-    goog.html.trustedresourceurl
-    goog.html.uncheckedconversions
-    goog.i18n.bidi
-    goog.i18n.bidiformatter
-    goog.i18n.charlistdecompressor
-    goog.i18n.charpickerdata
-    goog.i18n.collation
-    goog.i18n.compactnumberformatsymbols
-    goog.i18n.currency
-    goog.i18n.datetimeformat
-    goog.i18n.datetimeparse
-    goog.i18n.datetimepatterns
-    goog.i18n.datetimesymbols
-    goog.i18n.graphemebreak
-    goog.i18n.messageformat
-    goog.i18n.mime
-    goog.i18n.numberformat
-    goog.i18n.numberformatsymbols
-    goog.i18n.ordinalrules
-    goog.i18n.pluralrules
-    goog.i18n.timezone
-    goog.i18n.uchar.localnamefetcher
-    goog.i18n.uchar.namefetcher
-    goog.i18n.uchar.remotenamefetcher
-    goog.i18n.uchar
-    goog.i18n.ucharnames
-    goog.iter.iter
-    goog.json.evaljsonprocessor
-    goog.json.hybrid
-    goog.json.json
-    goog.json.nativejsonprocessor
-    goog.json.processor
-    goog.labs.useragent.browser
-    goog.labs.useragent.engine
-    goog.labs.useragent.platform
-    goog.labs.useragent.util
-    goog.locale.defaultlocalenameconstants
-    goog.locale.genericfontnames
-    goog.locale.locale
-    goog.locale.nativenameconstants
-    goog.locale.timezonedetection
-    goog.locale.timezonefingerprint
-    goog.log.log
-    goog.loremipsum.text.loremipsum
-    goog.math.affinetransform
-    goog.math.bezier
-    goog.math.box
-    goog.math.coordinate
-    goog.math.coordinate3
-    goog.math.exponentialbackoff
-    goog.math.integer
-    goog.math.interpolator.interpolator1
-    goog.math.interpolator.linear1
-    goog.math.interpolator.pchip1
-    goog.math.interpolator.spline1
-    goog.math.irect
-    goog.math.line
-    goog.math.long
-    goog.math.math
-    goog.math.matrix
-    goog.math.path
-    goog.math.paths
-    goog.math.range
-    goog.math.rangeset
-    goog.math.rect
-    goog.math.size
-    goog.math.tdma
-    goog.math.vec2
-    goog.math.vec3
-    goog.mochikit.async.deferred
-    goog.net.errorcode
-    goog.net.eventtype
-    goog.net.httpstatus
-    goog.net.wrapperxmlhttpfactory
-    goog.net.xhrio
-    goog.net.xhrlike
-    goog.net.xmlhttp
-    goog.net.xmlhttpfactory
-    goog.object.object
-    goog.promise.promise
-    goog.promise.resolver
-    goog.promise.thenable
-    goog.reflect.reflect
-    goog.spell.spellcheck
-    goog.string.const
-    goog.string.newlines
-    goog.string.parser
-    goog.string.string
-    goog.string.stringbuffer
-    goog.string.stringformat
-    goog.string.stringifier
-    goog.string.typedstring
-    goog.structs.avltree
-    goog.structs.circularbuffer
-    goog.structs.collection
-    goog.structs.heap
-    goog.structs.inversionmap
-    goog.structs.linkedmap
-    goog.structs.map
-    goog.structs.node
-    goog.structs.pool
-    goog.structs.prioritypool
-    goog.structs.priorityqueue
-    goog.structs.quadtree
-    goog.structs.queue
-    goog.structs.set
-    goog.structs.simplepool
-    goog.structs.stringset
-    goog.structs.structs
-    goog.structs.treenode
-    goog.structs.trie
-    goog.testing.asserts
-    goog.testing.jsunitexception
-    goog.testing.performancetable
-    goog.testing.pseudorandom
-    goog.testing.stacktrace
-    goog.timer.timer
-    goog.uri.uri
-    goog.uri.utils
-    goog.useragent.product
-    goog.useragent.useragent])

commit c55ec6a79cb6d49620d865c079b77ee151078bc3

Fix bugs in lazily loading source-maps
Fix bugs introduced with https://github.com/mfikes/planck/commit/97f1a0f47eb3b2f17a5c62ad9e74396566def0e8

planck-cljs/src/planck/repl.cljs
@@ -1076,22 +1076,22 @@
 
 (defn- load-bundled-source-maps!
   [ns-syms]
-  (when-not (get (:source-maps @planck.repl/st) 'cljs.core)
-    (let [source-map-path  (fn [ns-sym]
-                             (str (cljs.js/ns->relpath ns-sym) ".js.map"))
-          load-source-maps (fn [ns-sym]
-                             (when-not (get-in [:source-maps ns-sym] @st)
-                               (when-let [sm-text (->> ns-sym
-                                                    source-map-path
-                                                    js/PLANCK_LOAD
-                                                    first)]
-                                 ;; Detect if we have source maps in need of decoding
-                                 ;; or if they are AOT decoded.
-                                 (if (or (string/starts-with? sm-text "{\"version\"")
-                                         (string/starts-with? sm-text "{\n\"version\""))
-                                   (cljs/load-source-map! st ns-sym sm-text)
-                                   (swap! st assoc-in [:source-maps ns-sym] (transit-json->cljs sm-text))))))]
-      (run! load-source-maps ns-syms))))
+  (let [source-map-path  (fn [ns-sym]
+                           (str (cljs.js/ns->relpath ns-sym) ".js.map"))
+        load-source-maps (fn [ns-sym]
+                           (when-not (get-in @st [:source-maps ns-sym])
+                             (if-let [sm-text (->> ns-sym
+                                                  source-map-path
+                                                  js/PLANCK_LOAD
+                                                  first)]
+                               ;; Detect if we have source maps in need of decoding
+                               ;; or if they are AOT decoded.
+                               (if (or (string/starts-with? sm-text "{\"version\"")
+                                       (string/starts-with? sm-text "{\n\"version\""))
+                                 (cljs/load-source-map! st ns-sym sm-text)
+                                 (swap! st assoc-in [:source-maps ns-sym] (transit-json->cljs sm-text)))
+                               (swap! st assoc-in [:source-maps ns-sym] {}))))]
+    (run! load-source-maps ns-syms)))
 
 (defn- load-core-macros-source-maps! []
   (load-bundled-source-maps! '[cljs.core$macros]))