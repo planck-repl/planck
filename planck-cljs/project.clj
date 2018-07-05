55a51dc6e98d87d9739eab94a4c5600f2af79cab
Mike Fikes <mike@fikesfarm.com>
2018-07-04 22:04:57 -0400

    Add a project.clj that delegates to deps.edn (#763)

---
+++ planck-cljs/project.clj
@@ -0,0 +1,4 @@
+(defproject planck "0.0.0"
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
