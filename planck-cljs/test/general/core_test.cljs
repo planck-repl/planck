ce84c3220dee4b1c34d30243834bef5078d82190
Mike Fikes <mike@fikesfarm.com>
2018-09-02 10:12:57 -0400

    wip

--- planck-cljs/test/general/core_test.cljs
+++ planck-cljs/test/general/core_test.cljs
@@ -2,9 +2,13 @@
   "General tests to ensure Planck is operating correctly.
   These tests are not testing any specific Planck namespace."
   (:require-macros
-   [clojure.template :refer [do-template]])
+   [clojure.template :refer [do-template]]
+   [cljs.analyzer.macros]
+   [cljs.compiler.macros]
+   [cljs.env.macros])
   (:require
-   [cljs.test :refer-macros [deftest is]]))
+   [cljs.test :refer-macros [deftest is]]
+   [cljs.pprint]))
 
 (deftest do-template-test
   (is (= '(do (+ 4 2) (+ 5 3))
@@ -18,3 +22,9 @@
 
 (deftest clojurescript-version-test
   (is (some? *clojurescript-version*)))
+
+(deftest macros-loaded
+  (is (exists? cljs.analyzer.macros$macros/no-warn))
+  (is (exists? cljs.compiler.macros$macros/emit-wrap))
+  (is (exists? cljs.env.macros$macros/ensure))
+  (is (exists? cljs.pprint$macros/pp)))
