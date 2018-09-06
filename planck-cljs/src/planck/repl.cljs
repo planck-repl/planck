2014da4d470d4811f3907b35e5ae129d7e27d1a2
Mike Fikes <mike@fikesfarm.com>
2018-09-05 20:39:58 -0400

    Monkey patch target-specific array? and find-ns-obj (#801)
    Fixes #798

--- planck-cljs/src/planck/repl.cljs
+++ planck-cljs/src/planck/repl.cljs
@@ -28,6 +28,14 @@
    [planck.repl-resources :refer [repl-special-doc-map special-doc-map]]
    [planck.themes :refer [get-theme]]))
 
+;; Monkey patch target-specific core fns
+
+(set! array? (fn [x] (instance? js/Array x)))
+
+(set! find-ns-obj (fn [ns] (let [munged-ns (munge (str ns))
+                                 segs (.split munged-ns ".")]
+                             (find-ns-obj* goog/global segs))))
+
 (defn- distinct-by
   "Returns a lazy sequence of the elements of coll, removing any elements that
   return duplicate values when passed to a function f."
