0bf819dc2021aca3114b9bdd7773eedcbbcc99ac
Mike Fikes <mike@fikesfarm.com>
2024-03-24 08:41:53 -0400

    Make project.clj deps match deps.edn

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["planck-cljs/src"]
-  :dependencies [[org.clojure/clojurescript "1.11.60"]
+  :dependencies [[org.clojure/clojurescript "1.11.132"]
                  [org.clojure/core.rrb-vector "0.1.2"]
                  [fipp/fipp "0.6.24"]
                  [malabarba/lazy-map "1.3"]
                  [com.cognitect/transit-cljs "0.8.269"]
-                 [com.cognitect/transit-js "0.8.861"]])
+                 [com.cognitect/transit-js "0.8.874"]])
